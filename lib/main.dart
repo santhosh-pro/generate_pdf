ff01072eb0b1da23b82d57cc70e59fdbdf35c243
Santhosh <santhoshprogrammer94@gmail.com>
2020-11-13 11:37:31 +0530

    added csv export

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'dart:io';
 import 'dart:typed_data';
-
+import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
-
+import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:permission_handler/permission_handler.dart';
 import 'package:printing/printing.dart';
 
 void main() => runApp(MyApp());
@@ -19,7 +21,59 @@ class MyApp extends StatelessWidget {
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.print),
           tooltip: 'Print Document',
-          onPressed: () {
+          onPressed: () async {
+            // List<ReportModel> items=List<ReportModel>();
+            // ReportModel l1=ReportModel();
+            // l1.year=2020;
+            // l1.name="San";
+
+            //  ReportModel l2=ReportModel();
+            // l2.year=2021;
+            // l2.name="Sat";
+            // items.add(l1);
+            // items.add(l2);
+            //   List<List<dynamic>> mylist = [
+            //                 <String>[
+            //                   'Year',
+            //                   'Name',
+
+            //                 ],
+            //                 ...items.map((item) => [
+            //                       item.year,
+            //                       item.name,
+            //                     ]),
+            //               ];
+
+            //               String csv =
+            //                   const ListToCsvConverter().convert(mylist);
+            //               print('csv=$csv');
+
+            //               final String dir =
+            //                   (await getApplicationDocumentsDirectory()).path;
+            //               final String path =
+            //                   '/storage/emulated/0/Download/location_record.csv';
+            //               print(path);
+
+            //               final File file = File(path);
+            //               var status = await Permission.storage.status;
+            //               if (status.isUndetermined) {
+            //                 await Permission.storage.request();
+            //               }
+
+            //               if (status.isGranted)
+            //                 file.writeAsString(csv).then((value) {
+            //                   print('write file result $value');
+            //                 }).catchError((error) {
+            //                   print('write file error $error');
+            //                 });
+
+            // final stream = new Stream.fromIterable([
+            //   ['a', 'b'],
+            //   [1, 2]
+            // ]);
+            // final csvRowStream = stream.transform(new ListToCsvConverter());
+            // File f = new File("filename.csv");
+            // f.writeAsString(csvRowStream.toString());
             Printing.layoutPdf(
               onLayout: (PdfPageFormat format) {
                 return buildPdf(format);
@@ -34,7 +88,7 @@ class MyApp extends StatelessWidget {
     );
   }
 
-  Future<Uint8List> buildPdf(PdfPageFormat format) async{
+  Future<Uint8List> buildPdf(PdfPageFormat format) async {
     final pw.Document doc = pw.Document();
     doc.addPage(
       pw.Page(
@@ -50,5 +104,26 @@ class MyApp extends StatelessWidget {
       ),
     );
     return doc.save();
+
+    final String dir = (await getApplicationDocumentsDirectory()).path;
+    final String path = '/storage/emulated/0/Download/location_record.csv';
+
+    final File file = File(path);
+    var status = await Permission.storage.status;
+    if (status.isUndetermined) {
+      await Permission.storage.request();
+    }
+
+    if (status.isGranted)
+      file.writeAsBytes(doc.save()).then((value) {
+        print('write file result $value');
+      }).catchError((error) {
+        print('write file error $error');
+      });
   }
+}
+
+class ReportModel {
+  double year;
+  String name;
 }
